59e3788ab8b1adbfbecd3b0d8985888bc7613806
stefanopuzzuoli-lr <stefano.puzzuoli@liferay.com>
2020-07-03 12:04:40 +0100

    LGSCD-5 Reformat code

--- modules/commerce-account-logo-personalisation/src/main/java/com/liferay/commerce/account/logo/personalisation/internal/display/context/CommerceAccountDisplayContext.java
+++ modules/commerce-account-logo-personalisation/src/main/java/com/liferay/commerce/account/logo/personalisation/internal/display/context/CommerceAccountDisplayContext.java
@@ -14,42 +14,34 @@ import com.liferay.portal.kernel.webserver.WebServerServletTokenUtil;
 import javax.servlet.http.HttpServletRequest;
 
 public class CommerceAccountDisplayContext {
-	
-	public CommerceAccountDisplayContext(
-			CommerceAccountService commerceAccountService,
+
+	public CommerceAccountDisplayContext(CommerceAccountService commerceAccountService,
 			HttpServletRequest httpServletRequest) {
 
-			_commerceAccountService = commerceAccountService;
+		_commerceAccountService = commerceAccountService;
 
-			_commerceAccountRequestHelper = new CommerceAccountRequestHelper(
-				httpServletRequest);
-			
-			_commerceContext = (CommerceContext)httpServletRequest.getAttribute(
-					CommerceWebKeys.COMMERCE_CONTEXT);
+		_commerceAccountRequestHelper = new CommerceAccountRequestHelper(httpServletRequest);
 
-		}
+		_commerceContext = (CommerceContext) httpServletRequest.getAttribute(CommerceWebKeys.COMMERCE_CONTEXT);
 
-	
-	
+	}
+
 	public CommerceAccount getCurrentCommerceAccount() throws PortalException {
-		long commerceAccountId = ParamUtil.getLong(
-			_commerceAccountRequestHelper.getRequest(), "commerceAccountId");
+		long commerceAccountId = ParamUtil.getLong(_commerceAccountRequestHelper.getRequest(), "commerceAccountId");
 
 		if (commerceAccountId > 0) {
-			return _commerceAccountService.getCommerceAccount(
-				commerceAccountId);
+			return _commerceAccountService.getCommerceAccount(commerceAccountId);
 		}
 
 		return getCurrentAccount();
 	}
-	
+
 	protected CommerceAccount getCurrentAccount() throws PortalException {
 		return _commerceContext.getCommerceAccount();
 	}
-	
+
 	public String getLogo(CommerceAccount commerceAccount) {
-		ThemeDisplay themeDisplay =
-			_commerceAccountRequestHelper.getThemeDisplay();
+		ThemeDisplay themeDisplay = _commerceAccountRequestHelper.getThemeDisplay();
 
 		StringBundler sb = new StringBundler(5);
 
@@ -57,12 +49,11 @@ public class CommerceAccountDisplayContext {
 		sb.append("/organization_logo?img_id=");
 		sb.append(commerceAccount.getLogoId());
 		sb.append("&t=");
-		sb.append(
-			WebServerServletTokenUtil.getToken(commerceAccount.getLogoId()));
+		sb.append(WebServerServletTokenUtil.getToken(commerceAccount.getLogoId()));
 
 		return sb.toString();
 	}
-	
+
 	private final CommerceAccountRequestHelper _commerceAccountRequestHelper;
 	private final CommerceAccountService _commerceAccountService;
 	private final CommerceContext _commerceContext;
--- modules/commerce-account-logo-personalisation/src/main/java/com/liferay/commerce/account/logo/personalisation/portlet/CommerceAccountLogoPersonalisationPortlet.java
+++ modules/commerce-account-logo-personalisation/src/main/java/com/liferay/commerce/account/logo/personalisation/portlet/CommerceAccountLogoPersonalisationPortlet.java
@@ -2,9 +2,7 @@ package com.liferay.commerce.account.logo.personalisation.portlet;
 
 import com.liferay.commerce.account.logo.personalisation.constants.CommerceAccountLogoPersonalisationPortletKeys;
 import com.liferay.commerce.account.logo.personalisation.internal.display.context.CommerceAccountDisplayContext;
-import com.liferay.commerce.account.model.CommerceAccount;
 import com.liferay.commerce.account.service.CommerceAccountService;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -22,42 +20,30 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author stefa
  */
-@Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=commerce",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=CommerceAccountLogoPersonalisation",
-		"javax.portlet.init-param.template-path=/",
+@Component(immediate = true, property = { "com.liferay.portlet.display-category=commerce",
+		"com.liferay.portlet.header-portlet-css=/css/main.css", "com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=CommerceAccountLogoPersonalisation", "javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + CommerceAccountLogoPersonalisationPortletKeys.COMMERCEACCOUNTLOGOPERSONALISATION,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
-)
+		"javax.portlet.security-role-ref=power-user,user" }, service = Portlet.class)
 public class CommerceAccountLogoPersonalisationPortlet extends MVCPortlet {
-	
 
 	@Override
 	public void render(RenderRequest renderRequest, RenderResponse renderResponse)
 			throws IOException, PortletException {
 
-		CommerceAccountDisplayContext commerceAccountDisplayContext =
-			new CommerceAccountDisplayContext(
-				_commerceAccountService,
-				_portal.getHttpServletRequest(renderRequest));
-		
-		renderRequest.setAttribute(
-			WebKeys.PORTLET_DISPLAY_CONTEXT, commerceAccountDisplayContext);		
-		
+		CommerceAccountDisplayContext commerceAccountDisplayContext = new CommerceAccountDisplayContext(
+				_commerceAccountService, _portal.getHttpServletRequest(renderRequest));
+
+		renderRequest.setAttribute(WebKeys.PORTLET_DISPLAY_CONTEXT, commerceAccountDisplayContext);
+
 		super.render(renderRequest, renderResponse);
 	}
-	
+
 	@Reference
 	private CommerceAccountService _commerceAccountService;
-	
+
 	@Reference
 	private Portal _portal;
 }
